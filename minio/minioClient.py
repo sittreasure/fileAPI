95ebbf42087c7b0ee86cd4ad05edccdf636cd14f
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-19 13:25:08 +0700

    [ADD] create class minio client & constructor

---
+++ minio/minioClient.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from minio import Minio
+
+class minioClient:
+  __minio = None
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    minioUrl = getattr(settings, 'MINIO_URL')
+    minioAccessKey = getattr(settings, 'MINIO_ACCESS_KEY')
+    minioSecretKey = getattr(settings, 'MINIO_SECRET_KEY')
+    self.__minio = Minio(
+      minioUrl,
+      access_key=minioAccessKey,
+      secret_key=minioSecretKey,
+      secure=True
+    )
