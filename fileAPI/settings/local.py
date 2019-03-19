ffc4d8b5a514a7dfcf677eb4bcf8c4e9b9dfb5e5
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-19 13:24:36 +0700

    [UPDATE] add minio variable in setting

--- fileAPI/settings/local.py
+++ fileAPI/settings/local.py
@@ -12,3 +12,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+MINIO_URL = 'play.minio.io:9000'
+MINIO_ACCESS_KEY = 'Q3AM3UQ867SPQQA43P2F'
+MINIO_SECRET_KEY = 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
