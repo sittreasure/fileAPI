f186076bf5ee1f3df390ea82a0b34b0d3ad13a71
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-28 21:17:40 +0700

    [UPDATE] move database & minio detail to base

--- fileAPI/settings/local.py
+++ fileAPI/settings/local.py
@@ -2,23 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
 CORS_ORIGIN_WHITELIST = [
     'localhost:3000'
 ]
-
-MINIO_URL = 'play.minio.io:9000'
-MINIO_ACCESS_KEY = 'Q3AM3UQ867SPQQA43P2F'
-MINIO_SECRET_KEY = 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
-
-MINIO_BUCKET_NAME = 'hellomyminio'
