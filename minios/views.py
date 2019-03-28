46434e9e4c87c2d39fa2d8936a75428d899b55e4
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-28 21:32:58 +0700

    [UPDATE] change method get support get data

--- minios/views.py
+++ minios/views.py
@@ -3,7 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .minioClient import minioClient
-from .serializers import MinioMetadataSerializer
+from .serializers import MinioMetadataSerializer, MinioDataSerializer
+
+bucketName = 'mockjsp'
 
 class MinioFileSet(APIView):
   __minioClient = None
@@ -13,16 +15,23 @@ class MinioFileSet(APIView):
     self.__minioClient = minioClient()
 
   def get(self, request):
-    prefixName = request.GET.get('prefix_name')
-    bucketName = getattr(settings, 'MINIO_BUCKET_NAME')
-    listObjects = self.__minioClient.listFiles(bucketName, prefixName)
-    listNewObjects = list(map(
-      lambda object:
-        {
-          'object_name': object.object_name,
-          'is_dir': object.is_dir
-        },
-      listObjects
-    ))
-    result = MinioMetadataSerializer(listNewObjects, many=True).data
+    result = None
+    objectName = request.GET.get('object_name', '')
+    if len(objectName) == 0:
+      data = self.__minioClient.get(bucketName, objectName)
+      result = MinioDataSerializer(data, many=False).data
+      pass
+    else:
+      prefixName = request.GET.get('prefix_name')
+      listObjects = self.__minioClient.listFiles(bucketName, prefixName)
+      listNewObjects = list(map(
+        lambda object:
+          {
+            'object_name': object.object_name,
+            'is_dir': object.is_dir
+          },
+        listObjects
+      ))
+      result = MinioMetadataSerializer(listNewObjects, many=True).data
+      pass
     return Response(result)
