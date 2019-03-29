2867efff72972b6c3c83fd749b29b642af9db4af
gammyp <gamebep@gmail.com>
2019-03-29 15:28:55 +0700

    [ADD] database envrionment for read from .env

--- fileAPI/settings/local.py
+++ fileAPI/settings/local.py
@@ -1,14 +1,3 @@
 from .base import *
 
 ALLOWED_HOSTS = ['*']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
