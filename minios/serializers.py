f5103300dc6ee7dd55697dc6aa473616c99a36dc
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-08 22:03:56 +0700

    [UPDATE] change attribute format from snake case to camel case

--- minios/serializers.py
+++ minios/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 
 class MinioMetadataSerializer(serializers.Serializer):
-  object_name = serializers.CharField()
-  is_dir = serializers.BooleanField()
+  name = serializers.CharField()
+  isDir = serializers.BooleanField()
 
 class MinioDataSerializer(serializers.Serializer):
-  object_data = serializers.CharField()
+  data = serializers.CharField()
