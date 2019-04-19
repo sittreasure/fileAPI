bc8737af8a0e73b6da0bc050d9d672f8762776db
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-19 22:37:19 +0700

    [UPDATE] create class file type

--- file/models.py
+++ file/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class FileType(models.Model):
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=20)
+  regularExpression = models.TextField(db_column='regular_expression')
+  fileType = models.CharField(max_length=5, db_column='file_type')
+  initialCode = models.TextField(db_column='initial_code')
+  createdAt = models.DateTimeField(db_column='created_at', auto_now_add=True)
+  updatedAt = models.DateTimeField(db_column='updated_at', auto_now=True)
+
+  class Meta:
+    db_table = 'FileTypes'
+
+  def __str__(self):
+    return 'file type : {name}'.format(name=self.name)
