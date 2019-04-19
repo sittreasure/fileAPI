050df85f0fd37665ea022fee0506a287235587db
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-19 20:19:28 +0700

    [UPDATE] add app file into local app

--- fileAPI/settings/base.py
+++ fileAPI/settings/base.py
@@ -41,11 +41,12 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
 ]
 
 LOCAL_APPS = [
-    'minios'
+    'minios',
+    'file',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
