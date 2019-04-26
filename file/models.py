09ce5530bead892e159cb65daa689fd6dc508ea6
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-26 12:30:50 +0700

    [UPDATE] column initial code can null & blank

--- file/models.py
+++ file/models.py
@@ -5,7 +5,7 @@ class FileType(models.Model):
   name = models.CharField(max_length=20)
   regularExpression = models.TextField(db_column='regular_expression')
   fileType = models.CharField(max_length=5, db_column='file_type')
-  initialCode = models.TextField(db_column='initial_code')
+  initialCode = models.TextField(null=True, blank=True, db_column='initial_code')
   createdAt = models.DateTimeField(db_column='created_at', auto_now_add=True)
   updatedAt = models.DateTimeField(db_column='updated_at', auto_now=True)
 
