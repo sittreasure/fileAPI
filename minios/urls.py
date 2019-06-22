069795775331e8d84074c6c4877f48d40bbb47b6
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-06-22 21:53:43 +0700

    [UPDATE] add route for create bucket

--- minios/urls.py
+++ minios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MinioFileView
+from .views import MinioFileView, MinioBucketView
 
 app_name = 'minio'
 
 urlpatterns = [
-  path(r'file', MinioFileView.as_view())
+  path(r'bucket/', MinioBucketView.as_view()),
+  path(r'file/', MinioFileView.as_view())
 ]
