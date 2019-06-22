f75d584631d96223dd97e6743c127944dc489222
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-06-22 22:40:27 +0700

    [UPDATE] add route folder

--- minios/urls.py
+++ minios/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import MinioFileView, MinioBucketView
+from .views import MinioFileView, MinioBucketView, MinioFolderView
 
 app_name = 'minio'
 
 urlpatterns = [
   path(r'bucket/', MinioBucketView.as_view()),
+  path(r'folder/', MinioFolderView.as_view()),
   path(r'file/', MinioFileView.as_view())
 ]
